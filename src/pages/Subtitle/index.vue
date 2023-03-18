5a43ee23ffb5f172d504fca6f11a2af66e472596
gujijiu <gujijiu@qq.com>
2023-03-18 17:05:48 +0800

    去除无用注释

--- src/pages/Subtitle/index.vue
+++ src/pages/Subtitle/index.vue
@@ -94,55 +94,6 @@ onKeyStroke(['p','P'], (e) => {
 }, { eventName: 'keyup' })
 
 
-
-
-// const lang = ref('zh-CN')
-// function sample<T>(arr: T[], size: number) {
-//   const shuffled = arr.slice(0)
-//   let i = arr.length
-//   let temp: T
-//   let index: number
-//   while (i--) {
-//     index = Math.floor((i + 1) * Math.random())
-//     temp = shuffled[index]
-//     shuffled[index] = shuffled[i]
-//     shuffled[i] = temp
-//   }
-//   return shuffled.slice(0, size)
-// }
-// const colors = ['aqua', 'azure', 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow', 'transparent']
-// const grammar = `#JSGF V1.0; grammar colors; public <color> = ${colors.join(' | ')} ;`
-// const speech = useSpeechRecognition({
-//   lang,
-//   continuous: true,
-// })
-// const color = ref('transparent')
-// if (speech.isSupported.value) {
-//   // @ts-expect-error missing types
-//   const SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList
-//   const speechRecognitionList = new SpeechGrammarList()
-//   speechRecognitionList.addFromString(grammar, 1)
-//   speech.recognition!.grammars = speechRecognitionList
-//   watch(speech.result, () => {
-//     for (const i of speech.result.value.toLowerCase().split(' ').reverse()) {
-//       if (colors.includes(i)) {
-//         color.value = i
-//         break
-//       }
-//     }
-//   })
-// }
-// const sampled = ref<string[]>([])
-// const start = () => {
-//   color.value = 'transparent'
-//   speech.result.value = ''
-//   sampled.value = sample(colors, 5)
-//   speech.start()
-// }
-// const { isListening, isSupported, stop, result } = speech
-// const selectedLanguage = ref(lang.value)
-// watch(lang, lang => isListening.value ? null : selectedLanguage.value = lang)
-// watch(isListening, isListening => isListening ? null : selectedLanguage.value = lang.value)
 </script>
 
 <template>
