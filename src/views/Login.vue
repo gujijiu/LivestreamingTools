ed2b162ce569b33e501ed2557cd62a3d1ee34b2c
gujijiu <gujijiu@qq.com>
2023-04-06 23:42:57 +0800

    完善路由

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,21 +1,59 @@
 <script setup lang="ts">
-import { reactive, toRefs,markRaw } from 'vue'
+import { reactive, toRefs,markRaw,onMounted,onBeforeUnmount  } from 'vue'
 import { useStore } from '@/store'
-// import {  } from '@/router'
-import Header from "@/components/main/header.vue"
-import Footer from "@/components/main/footer.vue"
+import router from '@/router'
+import Header from "@/components/Header.vue"
+import Footer from "@/components/Footer.vue"
+
+onMounted(()=>{
+    // 添加背景
+    document.body.style.backgroundColor = '#f0f3f8';
+    // console.log(this)
+})
+  // 离开VUE时调用的钩子函数
+onBeforeUnmount(()=>{
+    // 清除背景图片
+    document.body.style.backgroundColor = ''
+})
 </script>
 
 <template>
-    <div>
-        <el-header><Header /></el-header>
-        <el-main>
-            
-        </el-main>
-        <el-footer><Footer /></el-footer>
+<div class="Login">
+    <div class="Login-header">
+        <div class="Login-tittle">
+            <span class="Tittle-span">欢迎登录</span>
+        </div>
     </div>
+    <div class="Login-main">
+        
+        <el-button @click="router.push({ name: 'Home' })">登录</el-button>
+    </div>
+    <div class="Login-footer">
+
+    </div>
+</div>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.Login{
+    width: 450px;
+    height: 350px;
+    background-color: aquamarine;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    -moz-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    -o-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+    .Login-tittle{
+        text-align: center;
+        
+        .Tittle-span{
+            font-size: 20px;
+            font-weight: 600;
+        }
+    }
+}
 </style>
